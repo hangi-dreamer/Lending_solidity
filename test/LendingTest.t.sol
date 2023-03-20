0c2d3f38bf4e7a124b8bd5d0840116a46dd68f12
hangi <hangi.kim@dream-academy.io>
2023-03-21 02:05:19 +0900

    init...

--- test/LendingTest.t.sol
+++ test/LendingTest.t.sol
@@ -12,25 +12,6 @@ contract CUSDC is ERC20 {
     }
 }
 
-contract DreamOracle {
-    address public operator;
-    mapping(address => uint256) prices;
-
-    constructor() {
-        operator = msg.sender;
-    }
-
-    function getPrice(address token) external view returns (uint256) {
-        require(prices[token] != 0, "the price cannot be zero");
-        return prices[token];
-    }
-
-    function setPrice(address token, uint256 price) external {
-        require(msg.sender == operator, "only operator can set the price");
-        prices[token] = price;
-    }
-}
-
 contract Testx is Test {
     DreamOracle dreamOracle;
     DreamAcademyLending lending;
